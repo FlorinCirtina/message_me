c86dd4cca773ec9c597bf96412e3e5fcb023547b
FlorinCirtina <florin.cirtina@gmail.com>
2021-01-08 10:54:43 +0200

    Enable flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,8 @@ require("semantic-ui-sass")
 //= require semantic-ui
 //= require_tree .
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
-
-
+$(document).on('turbolinks:load', function(){     
+	$(".ui.dropdown").dropdown();     
+	$('.message .close').on('click', function(){
+		$(this).closest('.message').transition('fade')}); 
+});
